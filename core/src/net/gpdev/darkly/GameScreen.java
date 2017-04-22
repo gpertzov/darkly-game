3229f185ec4235ac1439fe1f947ba247b5cc12d2
gpertzov <gamedev@gpdev.net>
2017-04-22 16:20:41 +0300

    Implement collision detection, Map edits, Minor refactoring

--- core/src/net/gpdev/darkly/GameScreen.java
+++ core/src/net/gpdev/darkly/GameScreen.java
@@ -10,15 +10,15 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
-
-import static com.badlogic.gdx.math.MathUtils.clamp;
 
 public class GameScreen extends ScreenAdapter {
 
@@ -28,9 +28,11 @@ public class GameScreen extends ScreenAdapter {
     private static final int VIEWPORT_HEIGHT = 16;
     private static final float UNIT_SCALE = 1 / 16f;
     private static final String POSITIONS_LAYER = "positions";
-    private static final String STRUCTURES_LAYER = "structures";
+    private static final String COLLISION_LAYER = "collision";
     private static final String PLAYER_START = "PLAYER";
     private static final float PLAYER_SPEED = 2;
+    private static final float PLAYER_WIDTH = 14;
+    private static final float PLAYER_HEIGHT = 16;
 
     private final DarklyGame game;
 
@@ -39,16 +41,14 @@ public class GameScreen extends ScreenAdapter {
     private SpriteBatch batch;
     private TiledMap map;
     private OrthogonalTiledMapRenderer mapRenderer;
+    private MapLayer collisionLayer;
     private TextureAtlas sprites;
-
-    private TiledMapTileLayer structuresLayer;
-    private int mapWidth = 0;
-    private int mapHeight = 0;
 
     // Player properties // TODO - Extract to class
     private Vector2 playerPosition;
     private Vector2 playerVelocity;
     private TextureRegion playerFrame;
+    private Rectangle playerBoundingBox;
 
 
     public GameScreen(final DarklyGame game) {
@@ -62,31 +62,22 @@ public class GameScreen extends ScreenAdapter {
 
         // Load map
         map = new TmxMapLoader().load("maps/prototype.tmx");
-        structuresLayer = (TiledMapTileLayer) map.getLayers().get(STRUCTURES_LAYER);
-        mapWidth = structuresLayer.getWidth();
-        mapHeight = structuresLayer.getHeight();
-
+        collisionLayer = map.getLayers().get(COLLISION_LAYER);
         mapRenderer = new OrthogonalTiledMapRenderer(map, UNIT_SCALE, batch);
+
+        // Load sprites
+        sprites = new TextureAtlas(Gdx.files.internal("art/sprites.atlas"));
 
         // Setup viewport
         camera = new OrthographicCamera();
         viewport = new ExtendViewport(VIEWPORT_WIDTH, VIEWPORT_HEIGHT, camera);
 
-        // Obtain player's start position
-        playerPosition = new Vector2(0, 0);
-        MapLayer positionsLayer = map.getLayers().get(POSITIONS_LAYER);
-        if (positionsLayer != null) {
-            RectangleMapObject playerStartRect = (RectangleMapObject) positionsLayer.getObjects().get(PLAYER_START);
-            if (playerStartRect != null) {
-                playerStartRect.getRectangle().getPosition(playerPosition);
-            }
-        }
+        // Init player entity
+        playerPosition = getMapPosition(PLAYER_START);
         playerPosition.scl(UNIT_SCALE);
         playerVelocity = new Vector2(0, 0);
-
-        // Load sprites
-        sprites = new TextureAtlas(Gdx.files.internal("art/sprites.atlas"));
         playerFrame = sprites.findRegion("player");
+        playerBoundingBox = new Rectangle(0, 0, PLAYER_WIDTH, PLAYER_HEIGHT / 2.0f);
 
         // Input processing // TODO - Extract to class
         Gdx.input.setInputProcessor(new InputAdapter() {
@@ -141,12 +132,15 @@ public class GameScreen extends ScreenAdapter {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        // Update player's position // TODO - Collision Detection
+        // Detect collisions with map
         final Vector2 velocity = playerVelocity.cpy().scl(delta);
         final Vector2 nextPosition = playerPosition.cpy().add(velocity);
-        nextPosition.x = clamp(nextPosition.x, 0, mapWidth - 1);
-        nextPosition.y = clamp(nextPosition.y, 0, mapHeight - 1);
-        playerPosition = nextPosition;
+        final boolean isCollision = isCollision(nextPosition);
+
+        // Update player's position
+        if (!isCollision) {
+            playerPosition = nextPosition;
+        }
 
         // Position camera over player position
         camera.position.set(playerPosition.x, playerPosition.y, 0f);
@@ -160,6 +154,42 @@ public class GameScreen extends ScreenAdapter {
         batch.begin();
         batch.draw(playerFrame, playerPosition.x, playerPosition.y, 1, 1);
         batch.end();
+    }
+
+    private Vector2 getMapPosition(final String positionId) {
+        Vector2 position = new Vector2(0, 0);
+        MapLayer positionsLayer = map.getLayers().get(POSITIONS_LAYER);
+        if (positionsLayer != null) {
+            try {
+                final MapObjects objects = positionsLayer.getObjects();
+                final RectangleMapObject positionRect = (RectangleMapObject) objects.get(positionId);
+                positionRect.getRectangle().getPosition(position);
+            } catch (Exception e) {
+                Gdx.app.error(TAG, "Failed to obtain position " + positionId);
+            }
+        } else {
+            Gdx.app.debug(TAG, "Map has no positions layer");
+        }
+        return position;
+    }
+
+    private boolean isCollision(Vector2 nextPosition) {
+        final Rectangle boundingBox = new Rectangle(nextPosition.x / UNIT_SCALE, nextPosition.y / UNIT_SCALE,
+                playerBoundingBox.width, playerBoundingBox.height);
+        if (collisionLayer != null) {
+            final MapObjects objects = collisionLayer.getObjects();
+            for (final MapObject object : objects) {
+                if (object instanceof RectangleMapObject) {
+                    final Rectangle rect = ((RectangleMapObject) object).getRectangle();
+                    if (boundingBox.overlaps(rect)) {
+                        return true;
+                    }
+                } else {
+                    Gdx.app.debug(TAG, "Unsupported collision object type");
+                }
+            }
+        }
+        return false;
     }
 
     @Override
