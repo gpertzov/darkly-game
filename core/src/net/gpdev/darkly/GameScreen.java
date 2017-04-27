9942f070cff05b103e7fa2dabc593e9a0d9f563a
gpertzov <gamedev@gpdev.net>
2017-04-28 01:18:02 +0300

    Implement lighting FBO, Fix player sprite margins, Collision boxes adjustments

--- core/src/net/gpdev/darkly/GameScreen.java
+++ core/src/net/gpdev/darkly/GameScreen.java
@@ -6,9 +6,12 @@ import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.maps.MapLayer;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.MapObjects;
@@ -26,10 +29,12 @@ public class GameScreen extends ScreenAdapter {
 
     private static final int VIEWPORT_WIDTH = 16;
     private static final int VIEWPORT_HEIGHT = 16;
-    private static final float UNIT_SCALE = 1 / 16f;
+    private static final int TILE_SIZE = 16;
+    private static final float UNIT_SCALE = 1.0f / TILE_SIZE;
     private static final String POSITIONS_LAYER = "positions";
     private static final String COLLISION_LAYER = "collision";
     private static final String PLAYER_START = "PLAYER";
+    private static final String PLAYER_SPRITE = "player";
     private static final float PLAYER_SPEED = 2;
     private static final float PLAYER_WIDTH = 14;
     private static final float PLAYER_HEIGHT = 16;
@@ -43,6 +48,10 @@ public class GameScreen extends ScreenAdapter {
     private OrthogonalTiledMapRenderer mapRenderer;
     private MapLayer collisionLayer;
     private TextureAtlas sprites;
+    private FrameBuffer lightBuffer;
+    private Texture lightTexture;
+
+    private Vector2 lightCenter;
 
     // Player properties // TODO - Extract to class
     private Vector2 playerPosition;
@@ -68,6 +77,9 @@ public class GameScreen extends ScreenAdapter {
         // Load sprites
         sprites = new TextureAtlas(Gdx.files.internal("art/sprites.atlas"));
 
+        // Load lightmaps
+        lightTexture = new Texture(Gdx.files.internal("art/light.png"));
+
         // Setup viewport
         camera = new OrthographicCamera();
         viewport = new ExtendViewport(VIEWPORT_WIDTH, VIEWPORT_HEIGHT, camera);
@@ -76,8 +88,10 @@ public class GameScreen extends ScreenAdapter {
         playerPosition = getMapPosition(PLAYER_START);
         playerPosition.scl(UNIT_SCALE);
         playerVelocity = new Vector2(0, 0);
-        playerFrame = sprites.findRegion("player");
-        playerBoundingBox = new Rectangle(0, 0, PLAYER_WIDTH, PLAYER_HEIGHT / 2.0f);
+        playerFrame = sprites.findRegion(PLAYER_SPRITE);
+        playerBoundingBox = new Rectangle(
+                (TILE_SIZE - PLAYER_WIDTH) / 2.0f, (TILE_SIZE - PLAYER_HEIGHT) / 2.0f,
+                PLAYER_WIDTH, PLAYER_HEIGHT / 2.0f);
 
         // Input processing // TODO - Extract to class
         Gdx.input.setInputProcessor(new InputAdapter() {
@@ -129,6 +143,23 @@ public class GameScreen extends ScreenAdapter {
 
     @Override
     public void render(float delta) {
+        // Prepare lighting FBO //
+        lightBuffer.begin();
+
+        batch.setBlendFunction(GL20.GL_SRC_ALPHA, GL20.GL_ONE);
+
+        // Ambient light color
+        Gdx.gl.glClearColor(0.1f, 0.1f, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // Render light map
+        batch.begin();
+        batch.draw(lightTexture, lightCenter.x, lightCenter.y);
+        batch.end();
+
+        lightBuffer.end();
+
+        // Render to display //
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
@@ -142,9 +173,12 @@ public class GameScreen extends ScreenAdapter {
             playerPosition = nextPosition;
         }
 
-        // Position camera over player position
-        camera.position.set(playerPosition.x, playerPosition.y, 0f);
+        // Center camera over player's position
+        camera.position.set(playerPosition.x + 0.5f, playerPosition.y + 0.5f, 0f);
         viewport.apply(false);
+
+        // Reset blending function
+        batch.setBlendFunction(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 
         // Render map
         mapRenderer.setView(camera);
@@ -154,6 +188,15 @@ public class GameScreen extends ScreenAdapter {
         batch.begin();
         batch.draw(playerFrame, playerPosition.x, playerPosition.y, 1, 1);
         batch.end();
+
+        // Blend lighting FBO
+        camera.setToOrtho(false);
+        batch.setProjectionMatrix(camera.combined);
+        batch.setBlendFunction(GL20.GL_DST_COLOR, GL20.GL_ZERO);
+        batch.begin();
+        batch.draw(lightBuffer.getColorBufferTexture(), 0, 0);
+        batch.end();
+
     }
 
     private Vector2 getMapPosition(final String positionId) {
@@ -174,9 +217,11 @@ public class GameScreen extends ScreenAdapter {
     }
 
     private boolean isCollision(Vector2 nextPosition) {
-        final Rectangle boundingBox = new Rectangle(nextPosition.x / UNIT_SCALE, nextPosition.y / UNIT_SCALE,
-                playerBoundingBox.width, playerBoundingBox.height);
         if (collisionLayer != null) {
+            final Rectangle boundingBox = new Rectangle(
+                    nextPosition.x / UNIT_SCALE + playerBoundingBox.x,
+                    nextPosition.y / UNIT_SCALE + playerBoundingBox.y,
+                    playerBoundingBox.width, playerBoundingBox.height);
             final MapObjects objects = collisionLayer.getObjects();
             for (final MapObject object : objects) {
                 if (object instanceof RectangleMapObject) {
@@ -195,10 +240,20 @@ public class GameScreen extends ScreenAdapter {
     @Override
     public void resize(int width, int height) {
         viewport.update(width, height);
+
+        // Create lighting FBO
+        if (lightBuffer != null) {
+            lightBuffer.dispose();
+        }
+        lightBuffer = new FrameBuffer(Pixmap.Format.RGBA8888, width, height, false);
+        lightCenter = new Vector2((lightBuffer.getWidth() - lightTexture.getWidth()) / 2.0f,
+                (lightBuffer.getHeight() - lightTexture.getHeight()) / 2.0f);
     }
 
     @Override
     public void dispose() {
+        lightBuffer.dispose();
+        lightTexture.dispose();
         sprites.dispose();
         map.dispose();
         batch.dispose();
