f8581fa6206e0b9ee7afbd7e92d7dd63f4667796
Gideon Pertzov <gamedev@gpdev.net>
2018-04-21 17:57:51 +0300

    Add DiceRoller and unit test, Move core src files to src/main/java

---
+++ core/src/main/java/net/gpdev/darkly/DiceRoller.java
@@ -0,0 +1,25 @@
+package net.gpdev.darkly;
+
+import com.badlogic.gdx.math.RandomXS128;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+import static java.util.stream.Collectors.toList;
+
+public class DiceRoller {
+    final private Random random;
+
+
+    public DiceRoller(final long seed) {
+        random = new RandomXS128(seed);
+    }
+
+    List<Integer> roll(final int numDice, final int numSides) {
+        return IntStream
+                .generate(() -> random.nextInt(numSides) + 1)
+                .limit(numDice)
+                .boxed().collect(toList());
+    }
+}
---
+++ core/src/test/java/net/gpdev/darkly/DiceRollerTest.java
@@ -0,0 +1,48 @@
+package net.gpdev.darkly;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class DiceRollerTest {
+
+    private static final int REPETITIONS = 100000;
+    private DiceRoller diceRoller;
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+        diceRoller = new DiceRoller(0);
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+        diceRoller = null;
+    }
+
+    @Test
+    public void testRoll1d20() {
+        for (int i = 0; i < REPETITIONS; i++) {
+            List<Integer> results = diceRoller.roll(1, 20);
+            assertEquals(1, results.size());
+            final Integer roll = results.get(0);
+            assertTrue(roll >= 1);
+            assertTrue(roll <= 20);
+        }
+    }
+
+    @Test
+    public void testRoll3d6() {
+        for (int i = 0; i < REPETITIONS; i++) {
+            List<Integer> results = diceRoller.roll(3, 6);
+            assertEquals(3, results.size());
+           results.forEach(roll -> {
+               assertTrue(roll >= 1);
+               assertTrue(roll <= 6);
+           });
+        }
+    }
+
+}
