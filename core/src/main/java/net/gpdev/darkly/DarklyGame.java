5d7f10d63d74ff8bbdb0d6b8b06d5f025203ce64
Gideon Pertzov <gamedev@gpdev.net>
2018-10-08 00:18:20 +0300

    Remove unused resources

--- core/src/main/java/net/gpdev/darkly/DarklyGame.java
+++ core/src/main/java/net/gpdev/darkly/DarklyGame.java
@@ -1,20 +1,13 @@
 package net.gpdev.darkly;
 
 import com.badlogic.gdx.Game;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class DarklyGame extends Game {
-
-    private SpriteBatch batch;
-    private BitmapFont font;
 
     public static final String FLASHLIGHT = "flashlight";
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        font = new BitmapFont();
         this.setScreen(new GameScreen(this));
     }
 
@@ -26,7 +19,5 @@ public class DarklyGame extends Game {
     @Override
     public void dispose() {
         super.dispose();
-        batch.dispose();
-        font.dispose();
     }
 }
