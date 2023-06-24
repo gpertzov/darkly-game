30d39e58345757c7edfa9748fdea88466ef72765
gpertzov <gpertzov@users.noreply.github.com>
2023-06-24 10:07:45 +0300

    Bump gdx version, LWGJL3

--- desktop/src/net/gpdev/darkly/desktop/DesktopLauncher.java
+++ desktop/src/net/gpdev/darkly/desktop/DesktopLauncher.java
@@ -1,12 +1,12 @@
 package net.gpdev.darkly.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import net.gpdev.darkly.DarklyGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new DarklyGame(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new DarklyGame(), config);
 	}
 }
