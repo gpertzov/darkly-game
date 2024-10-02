6fd7f20c5f0bf131ce42652de7db8c2b1ba11d52
gpertzov <gpertzov@users.noreply.github.com>
2024-10-02 13:21:37 +0300

    Set window size

--- desktop/src/net/gpdev/darkly/desktop/DesktopLauncher.java
+++ desktop/src/net/gpdev/darkly/desktop/DesktopLauncher.java
@@ -7,6 +7,7 @@ import net.gpdev.darkly.DarklyGame;
 public class DesktopLauncher {
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+        config.setWindowedMode(800, 600);
         new Lwjgl3Application(new DarklyGame(), config);
     }
 }
